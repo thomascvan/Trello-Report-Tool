85486ec9a2b5b3c53b1be7fe43aa3094edfd89e0
Thomas Van <thomascvan11@gmail.com>
2023-01-18 11:51:54 -0800

    added dictionary object for lists on board

--- report.js
+++ report.js
@@ -1,5 +1,17 @@
 var fs = require('fs');
 const prompt = require("prompt-sync")({ sigint: true });
+
+// List dictionary
+{
+  'Engineering Queue': '635a82b77dc34d052c428471',
+  'Expedites': '636aaa703778a90550a5ca95',
+  'Layout': '635ac9c199d5fd02ce66210d',
+  'In Design': '635a82c0222dd201c3566620',
+  'Need/Missing Info From Sales': '638535f64fd75d00b4c284b3',
+  'Data Entry': '635a82e622836b00d0ab9e43',
+  'Released': '63921ed2610c4900859fa15d'
+}
+
 var obj = JSON.parse(fs.readFileSync('./data/data.json', 'utf8'));
 
 const input = prompt("What date? (Format MM-DD-YYYY) ");
@@ -7,6 +19,7 @@ const date = `${input.slice(-4)}-${input.slice(0,2)}-${input.slice(3,5)}`
 console.log(`Searching date ${date}`);
 
 var counter = 0;
+
 for (var i = 0; i < obj.actions.length; i++) {
   if (obj.actions[i].data.listBefore != undefined) {
 
