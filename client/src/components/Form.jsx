81b4944f3b63e4900662b569fee66c09f58e17ba
Van <thomas@rwbteam.com>
2023-01-19 09:13:42 -0800

    fix deprication issues

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Form = () => {
 
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+
   return (
 <div className="attendee-form">
         <label>Start Date:
-          <input type="date" onChange={e => setStartDate(e.target.value[0].toUpperCase() + e.target.value.substring(1))} ></input>
+          <input type="date" onChange={e => setStartDate(e.target.value)} ></input>
         </label>
         <label>End Date:
-          <input type="date" onChange={e => setEndDate(e.target.value[0].toUpperCase() + e.target.value.substring(1))} ></input>
+          <input type="date" onChange={e => setEndDate(e.target.value)} ></input>
         </label>
         <button onClick={() => formSubmit() }>Run Report</button>
     </div>
