f7af997cdef0b2769616fd4a84963c509a4f7a8c
Van <thomas@rwbteam.com>
2023-03-09 10:29:35 -0800

    further development of bar chart

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,11 +7,14 @@ class App extends Component {
 
     render() {
         return (
+            <>
             <div>
                 <h1>JE Engineering Output Report</h1>
                 <Form />
-                <BarChart />
             </div>
+            <div className="chart-container" style={{position: 'relative', height: '40vh', width:'70vw'}}> <h6>STILL TESTING, NOT ACCURATE:</h6> <BarChart /> </div>
+            {/* <BarChart style={{position: 'relative', height: '40vh', width:'80vw'}}/> */}
+            </>
         )
     }
 }
--- client/src/components/BarChart.jsx
+++ client/src/components/BarChart.jsx
@@ -2,30 +2,33 @@ import React from 'react';
 import { Bar } from 'react-chartjs-2';
 import {Chart as ChartJS} from 'chart.js/auto';
 
+let objData = {
+      "ricardopuente": 38,
+      "mattpericoni1": 23,
+      "michalhastings": 11,
+      "thomas_van": 18,
+      "alexanderdabrowski": 4,
+      "danielrice12": 7,
+      "ezramalernee": 1,
+      "davidback5": 6,
+      "claytonbrown40": 1
+    };
+
+let keys = Object.keys(objData);
+let values = Object.values(objData);
+
 let Data = {
-    labels: ["ricardopuente", "mattpericoni1"], 
+    labels: keys, 
     datasets: [{
         label: "Jobs Engineered",
-        data: [38, 23]
+        data: values
     }]
 };
 
-// let Data = [
-//     {
-//       "ricardopuente": 38,
-//       "mattpericoni1": 23,
-//       "michalhastings": 11,
-//       "thomas_van": 18,
-//       "alexanderdabrowski": 4,
-//       "danielrice12": 7,
-//       "ezramalernee": 1,
-//       "davidback5": 6,
-//       "claytonbrown40": 1
-//     }
-
-//   ];
 function BarChart() {
-    return <Bar data={Data} />
+    // return <Bar data={Data} width={"30%"} options={{ maintainAspectRatio: false }}/>
+    // return <Bar data={Data} width={"30%"} height={"10%"} />
+    return <Bar data={Data} options={{ maintainAspectRatio: false }}/>
 }
 
 export default BarChart;
